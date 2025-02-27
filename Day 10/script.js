2b188d4ffb4bcea89c367496bd8c6e77b996dc99
Devyani <devyani0096@gmail.com>
2025-02-27 16:52:02 +0530

    Day 10

---
+++ Day 10/script.js
@@ -0,0 +1,69 @@
+// const getData=(text)=>{
+//     const pr=fetch('https://dummyjson.com/recipes/search?q=Margherita');
+//     pr.then((res)=>{
+//         const pr2=res.json();
+//         pr2.then((data)=>{
+//             showCards(data.recipes);
+//         });
+//     });
+// };
+
+// const root=document.getElementById('cards-container');
+
+// const showCards=(dataArr)=>{
+//     root.innerHTML="";
+//     dataArr.forEach((ele)=>{
+//         const newDiv=document.createElement('div');
+//         newDiv.className='card';
+//         newDiv.innerHTML=`
+//             <h4>${ele.name}</h4>
+//             <img src="${ele.image}" width="100px"/>
+//             <p>${ele.cuisine}</p>
+//         `;
+//         root.appendChild(newDiv);
+//     });
+// };
+
+// const handleSearch=(e)=>{
+//     getData(e.target.value);
+// }
+
+// Debounce
+
+const getData=(text)=>{
+    const pr=fetch(`https://dummyjson.com/recipes/search?q=${text}`);
+    pr.then((res)=>{
+        const pr2=res.json();
+        pr2.then((data)=>{
+            showCards(data.recipes);
+        });
+    });
+};
+
+const root=document.getElementById('cards-container');
+
+const showCards=(dataArr)=>{
+    root.innerHTML="";
+    dataArr.forEach((ele)=>{
+        const newDiv=document.createElement('div');
+        newDiv.className='card';
+        newDiv.innerHTML=`
+            <h4>${ele.name}</h4>
+            <img src="${ele.image}" width="100px"/>
+            <p>${ele.cuisine}</p>
+        `;
+        root.appendChild(newDiv);
+    });
+};
+
+let timeoutId=null;
+
+const handleSearch=(e)=>{
+    if(timeoutId)
+    {
+        clearTimeout(timeoutId);
+    }
+    timeoutId=setTimeout(()=>{
+        getData(e.target.value);
+    },1000);
+};
